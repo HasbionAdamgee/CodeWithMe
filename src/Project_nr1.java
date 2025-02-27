33c0123ae85ccfcdd4ce57b9c345518a8a954141
HasbionAdamgee <hmucabe1@stu.vistula.edu.pl>
2025-02-27 22:47:47 +0100

    Proceed or not

--- src/Project_nr1.java
+++ src/Project_nr1.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Project_nr1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String name;
+        String name, response;
         short age ;
         System.out.println("What is your name?");
         name= br.readLine();
@@ -16,7 +16,19 @@ public class Project_nr1 {
             System.out.println("You are only eligible if you are above 18");
         }else{
             System.out.println("You are eligible");}
-        System.out.println("Go to the next counter!");
+        System.out.println("Do you want to go to the next counter? (yes/no)");
+        response = br.readLine();
+
+        response = response.toLowerCase();
+
+        if (response.equals("yes")) {
+            System.out.println("Go to the next counter.");
+        } else if (response.equals("no")) {
+            System.out.println("Stay here.");
+        } else {
+            System.out.println("Invalid answer, type yes or no.");
+        }
+
 
 
     }

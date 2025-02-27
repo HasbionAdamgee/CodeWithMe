a098e8768b864d7fe0e0a946e657e13779f318ee
HasbionAdamgee <hmucabe1@stu.vistula.edu.pl>
2025-02-27 22:16:45 +0100

    Age eligibility

--- src/Project_nr1.java
+++ src/Project_nr1.java
@@ -6,8 +6,16 @@ public class Project_nr1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String name;
+        short age ;
         System.out.println("What is your name?");
         name= br.readLine();
         System.out.println("Welcome To Vistula "+name);
+        System.out.println("Are you above 18?");
+        age = Short.parseShort(br.readLine());
+        if (age < 18) {
+            System.out.println("You are only eligible if you are above 18");
+        }else{
+            System.out.println("You are eligible");}
+
     }
 }
